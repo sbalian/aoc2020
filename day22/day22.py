c48468d40d96386ef71d589184c64d5747dfa482
Seto Balian <seto.balian@gmail.com>
2020-12-22 22:49:32 +0000

    Day 22 part 1

---
+++ day22/day22.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+
+def read_input(path):
+    with open(path) as f:
+        player1, player2 = f.read().strip().split("\n\n")
+    player1 = [int(x) for x in player1.split("\n")[1:]]
+    player2 = [int(x) for x in player2.split("\n")[1:]]
+    return player1, player2
+
+
+def score(player):
+    n = len(player)
+    return sum([player[n - i] * i for i in reversed(range(1, n + 1))])
+
+
+def part1(player1, player2):
+    while player1 != [] and player2 != []:
+        card1 = player1.pop(0)
+        card2 = player2.pop(0)
+        if card1 > card2:
+            player1.extend([card1, card2])
+        else:
+            player2.extend([card2, card1])
+    if player1 == []:
+        return score(player2)
+    else:
+        return score(player1)
+
+
+def main():
+    player1, player2 = read_input("input.txt")
+    assert part1(player1, player2) == 32472
+    print("All tests passed.")
+
+
+if __name__ == "__main__":
+    main()
